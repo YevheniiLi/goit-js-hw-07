3bf05f496272d93570b902e3498b882262dcc3d2
Yevhenii Lymarenko <jekilllimarenko@gmail.com>
2022-06-03 18:23:05 +0300

    fix galerry.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,88 +4,114 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+
+
+
+
+
 const refs = {
-    galleryList: document.querySelector('ul.gallery'),
-    lightbox: document.querySelector('.lightbox'),
-    btn: document.querySelector('[data-action="close-lightbox"]')
+    gallery: document.querySelector('.gallery'),
+    wrapper: document.querySelector('.lightbox'),
+    backdrop: document.querySelector('.lightbox__overlay'),
+    wrapperContent: document.querySelector('.lightbox__content'),
+    wrapperImg: document.querySelector('.lightbox__image'),
+    closeWrapperButton: document.querySelector('.lightbox__button'),
   };
   
-  document.querySelector('button.image').onclick = () => {
-
-	basicLightbox.create(`
-		<img width="1400" height="900" src="https://placehold.it/1400x900">
-	`).show()
-
-}
-
+  /*Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.*/
   
-     
+  const galleryItemsMarkup = createElGalleryMarkup(galleryItems);
+  refs.gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
   
-  const createImage = (item, parent) => {
-    const { preview, original, description } = item;
-    const img = document.createElement('img');
-    
-    img.classList.add('gallery__image');
-    img.dataset.source = original;
-    img.src = preview;
-    img.alt = description;
-    
-    parent.appendChild(img);
-  };
-  
-  const createLink = (item, parent) => {
-    const { original } = item;
-    const a = document.createElement('a');
-    
-    a.classList.add('gallery__link');
-    a.href = original;
-    
-    createImage(item, a);
-    
-    parent.appendChild(a);
-  };
-  
-  const createItem = (item) => {
-    const li = document.createElement('li');
-    li.classList.add('gallery__item');
-    
-    createLink(item, li);
-    
-    return li;
-  };
-  
-  const renderListItems = (arr) => {
-    const items = arr.map( (item) => createItem(item));
-    
-    refs.galleryList.append(...items);
-  };
-  
-  renderListItems(images);
-  
-  
-  
-  
-  function onClickHandler(e) {
-    e.preventDefault();
-    
-    if(e.target.nodeName === 'IMG') {
-      refs.lightbox.classList.add('is-open');
-      refs.lightbox.querySelector('.lightbox__image').src = e.target.src;
-      refs.lightbox.querySelector('.lightbox__image').alt = e.target.alt;
-    }
+  function createElGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+      <li class = "gallery__item">
+        <a
+          class = "gallery__link"
+          href = ${original}
+        >
+          <img
+            loading="lazy"
+            class = "gallery__image"
+            src = "${preview}"
+            data-source = "${original}"
+            alt = "${description}"
+          />
+        </a>
+      </li>
+      `;
+      })
+      .join('');
   }
   
-  function onCloseHandler(e) {
-    if(e.target.nodeName === "I" || e.target.nodeName === "BUTTON") {
-      refs.lightbox.classList.remove('is-open');
+  /* Реализация делегирования на галерее ul.js-gallery и получение url большого изображения */
+  
+  refs.gallery.addEventListener('click', onGalleryItemClick);
+  
+  function onGalleryItemClick(evt) {
+    /*  Отмена действия по умолчанию при клике на ссылку */
+    evt.preventDefault();
+    /* ****** */
+  
+    const isGalleryEl = evt.target.classList.contains('gallery__image');
+    if (!isGalleryEl) {
+      return;
     }
+  
+    /* Открытие модального окна по клику на элементе галереи. */
+    window.addEventListener('keydown', onEscPress);
+    refs.wrapper.classList.add('is-open');
+  
+    /* Подмена значения атрибута src, alt элемента img.lightbox__image. */
+    refs.wrapperImg.src = evt.target.dataset.source;
+    refs.wrapperImg.alt = evt.target.alt;
+
   }
   
-  refs.galleryList.addEventListener('click', onClickHandler);
-  refs.btn.addEventListener('click', onCloseHandler);
-
-
-
-
+  /* Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]. */
+  
+  refs.closeWrapperButton.addEventListener('click', onCloseWrapper);
+  
+  function onCloseWrapper() {
+    refs.wrapper.classList.remove('is-open');
+    window.removeEventListener('keydown', onEscPress);
+  
+    /* Очистка значения атрибута src элемента img.lightbox__image. */
+    refs.wrapperImg.src = '';
+  }
+  
+  /* Закрытие модального окна по клику на div.lightbox__overlay. */
+  
+  refs.backdrop.addEventListener('click', onBackdropClick);
+  
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      onCloseWrapper();
+    }
+     }
+  
+  /* Закрытие модального окна по нажатию клавиши ESC. */
+  
+  function onEscPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+  
+    if (event.code === ESC_KEY_CODE) {
+      onCloseWrapper();
+    }  
+  }
+  
+  /* Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
+  let arr = [];
+  for (let child of refs.gallery.children) {
+    //console.log(child.previousElementSibling);
+    arr.push(child);
+  
+    //console.log(child.querySelector('.gallery__image').dataset.source);
+  }
+  console.log(arr[0]);
+  
+  // console.log(refs.gallery.firstElementChild);
+  // console.log(refs.gallery.lastElementChild);
 
   

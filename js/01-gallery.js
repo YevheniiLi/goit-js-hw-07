524d5a305777c58b69dd118cff2a11ed9fc72ed2
Yevhenii Lymarenko <jekilllimarenko@gmail.com>
2022-06-04 18:19:51 +0300

    fixed some mistakes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,108 +4,54 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-const refs = {
-    gallery: document.querySelector('.gallery'),
-    wrapper: document.querySelector('.lightbox'),
-    backdrop: document.querySelector('.lightbox__overlay'),
-    wrapperContent: document.querySelector('.lightbox__content'),
-    wrapperImg: document.querySelector('.lightbox__image'),
-    closeWrapperButton: document.querySelector('.lightbox__button'),
-  };
-  
-  /*Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.*/
-  
-  const galleryItemsMarkup = createGalleryMarkup(galleryItems);
-  refs.gallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
-  
+
+//   /*Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.*/
+
+const galleryItemsMarkup = document.querySelector('.gallery');
+createGalleryMarkup(galleryItems);
+
   function createGalleryMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
+    const newGalleryMarkup = galleryItems.map(({ preview, original, description }) => {
         return `
-      <li class = "gallery__item">
-        <a
-          class = "gallery__link"
-          href = ${original}
-        >
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
           <img
-            loading="lazy"
-            class = "gallery__image"
-            src = "${preview}"
-            data-source = "${original}"
-            alt = "${description}"
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
           />
         </a>
-      </li>
-      `;
+      </div>       
+      `   
       })
       .join('');
+      galleryItemsMarkup.insertAdjacentHTML('beforeend', newGalleryMarkup);
   }
   
-  /*Подключение скрипта и стилей библиотеки модального окна BasiclightBox.
-    Открытие модального окна  */
+//   /*Подключение скрипта и стилей библиотеки модального окна BasiclightBox.
+//     Открытие модального окна  */
   
-  refs.gallery.addEventListener('click', onGalleryItemClick);
+  galleryItemsMarkup.addEventListener('click', onGalleryItemClick);
 
   function onGalleryItemClick(event) {
-    event.preventDefault();
+     event.preventDefault();
 
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
-
-    if (event.target.nodeName === "IMG") {
-        const instance = basicLightbox.create(`
+     const instance = basicLightbox.create(`
         <img src="${event.target.dataset.source}" width="800" height="600">
         `);
         instance.show();
-    }
-    window.addEventListener('keyup', onCloseModal);
-  }
+        document.addEventListener('keydown', onEscPress)
+  
+//   // /* Закрытие модального окна по нажатию клавиши ESC. */
 
-
-  // /* Закрытие модального окна по клику на кнопку */
-  
-  refs.closeWrapperButton.addEventListener('click', onCloseWrapper);
-  
-  function onCloseWrapper() {
-    refs.wrapperImg.src = '';
-    refs.wrapper.classList.remove('is-open');
-    window.removeEventListener('keydown', onEscPress); 
-  }
-  
-  /* Закрытие модального окна по клику  */
-  
-  refs.backdrop.addEventListener('click', onBackdropClick);
-  
-  function onBackdropClick(event) {
-    if (event.target === event.currentTarget) {
-      onCloseWrapper();
-    }
-     }
-  
-  // /* Закрытие модального окна по нажатию клавиши ESC. */
-  
-  function onEscPress(event) {
-    const ESC_KEY_CODE = 'Escape';
-  
-    if (event.code === ESC_KEY_CODE) {
-      onCloseWrapper();
+function onEscPress(event) {
+      if (event.code === 'Escape') {
+     instance.close();
+     document.removeEventListener('keydown', onEscPress)
     }  
   }
-  
-  /* Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо". */
-  let arr = [];
-  for (let child of refs.gallery.children) {
-    //console.log(child.previousElementSibling);
-    arr.push(child);
-  
-    //console.log(child.querySelector('.gallery__image').dataset.source);
-  }
-  console.log(arr[0]);
-  
-  // console.log(refs.gallery.firstElementChild);
-  // console.log(refs.gallery.lastElementChild);
-
-  
+}
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryItemsMarkup = document.querySelector(".gallery");
+
+createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  const newGalleryMarkup = galleryItems.map(({preview, original, description}) =>
+        `<li><a class="gallery__item" 
+        href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" alt="${description}"/>
+        </a>
+        </li>`
+    )
+    .join("");
+  galleryItemsMarkup.insertAdjacentHTML("beforeend", newGalleryMarkup);
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
